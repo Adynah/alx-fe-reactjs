ba07db41ab36dba7deda494c6c14a9cea0eaabae
adynah <t.barimalaa@gmail.com>
2025-07-12 02:16:06 +0100

    Created Specific Components in a React Application - Header, MainContent and Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -20,6 +23,9 @@ function App() {
 
       <div>
         <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
       </div>
     </>
   );
