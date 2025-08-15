cae79ce419fbadea54922bf6bf24b7d3275ecd0a
adynah <t.barimalaa@gmail.com>
2025-08-16 00:33:05 +0100

    Added Advanced Search Features with tailwind

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 import Search from "./components/Search";
 import UserCard from "./components/UserCard";
-import { getUser } from "./services/githubService";
-import './App.css'
+import { fetchUserData } from "./services/githubService";
+import './index.css';
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleSearch = async (username) => {
+    try {
+      const data = await fetchUserData(username);
+      setUser(data);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+      setUser(null);
+    }
+  };
 
   return (
     <>
       <div>
         <h1>Welcome to GitHub User Search</h1>
-        <Search />
-        <UserCard user={user} />
+        <Search onSearch={handleSearch} />
+        {user && <UserCard user={user} />}
       </div>
       
     </>
