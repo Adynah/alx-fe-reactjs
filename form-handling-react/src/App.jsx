15201a6c31110140fb5b3b2617da07107678a0f2
adynah <t.barimalaa@gmail.com>
2025-09-10 11:31:22 +0100

    Rename FormikForm.jsx to formikForm.js

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <RegistrationForm />
-      <FormikForm />
+      <formikForm />
     </div>
   );
 }
